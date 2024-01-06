cdf8aca0947a68d0a72f30766caf29a827b1c63e
Son Hong Do <sondh0127@gmail.com>
2024-01-06 15:03:25 +0700

    style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,20 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@vue-macros/nuxt',
+    "@nuxt/image",
+    '@unocss/nuxt',
+    '@nuxtjs/google-fonts',
   ],
   macros: {
     betterDefine: false,
+  },
+  css: [
+    '@unocss/reset/tailwind-compat.css'
+  ],
+  googleFonts: {
+    families: {
+      Inter: ['100..800'],
+    },
+    download: true,
   },
 })
