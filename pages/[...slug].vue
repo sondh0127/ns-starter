2f608b4f24a16067043ddf4738ceabefdbed99ef
Son Hong Do <sondh0127@gmail.com>
2024-01-07 08:42:53 +0700

    new nav

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 const { params } = useRoute()
-console.log('[LOG] ~ file: [...slug].vue:3 ~ params:', params)
+
+const isResources = params.slug.includes('resources')
+console.log('[LOG] ~ file: [...slug].vue:6 ~ isResources:', isResources)
 </script>
 
 <template>
-  <NuxtLayout>
+  <NuxtLayout :name="isResources ? 'resources' : 'default'">
     <ContentDoc />
   </NuxtLayout>
 </template>
